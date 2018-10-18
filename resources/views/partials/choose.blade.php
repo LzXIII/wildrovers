dcfdb9732f6a3f273ac8be57a28561478e03ddb9
LzXIII <campanellalorenzo@gmail.com>
2018-10-18 10:32:20 +0200

    metà ottobre

---
+++ resources/views/partials/choose.blade.php
@@ -0,0 +1,48 @@
+<!--Choose section-->
+<section id="choose" class="choose bg-grey roomy-80">
+    <div class="container">
+        <div class="row">
+            <div class="main_choose">
+                <div class="col-md-6">
+                    <div class="choose_mac">
+                        <div class="choose_slide">
+                            <div class="choose_item">
+                                <img src="assets/images/choose-img1.png" alt="" />
+                            </div>
+                            <div class="choose_item">
+                                <img src="assets/images/choose-img1.png" alt="" />
+                            </div>
+                            <div class="choose_item">
+                                <img src="assets/images/choose-img1.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- End off col-md-6 -->
+
+                <div class="col-md-5 col-md-offset-1">
+                    <div class="choose_content sm-m-top-40">
+                        <div class="head_title">
+                            <h2 class="text-uppercase">Why <strong>Choose Us</strong></h2>
+                        </div>
+                        <div class="choose_item_text fix">
+                            <h6><i class="fa fa-check-square-o"></i> Easy to costumize.</h6>
+                            <p>Atque ducimus velit, earum quidem, iusto dolorem. </p>
+                        </div>
+                        <div class="choose_item_text fix m-top-20">
+                            <h6><i class="fa fa-check-square-o"></i> Full of features.</h6>
+                            <p>Atque ducimus velit, earum quidem, iusto dolorem. </p>
+                        </div>
+                        <div class="choose_item_text fix m-top-20">
+                            <h6><i class="fa fa-check-square-o"></i> 24/7 Support.</h6>
+                            <p>Atque ducimus velit, earum quidem, iusto dolorem. </p>
+                        </div>
+                        <div class="choose_item_text fix m-top-20">
+                            <h6><i class="fa fa-check-square-o"></i> All in psd files.</h6>
+                            <p>Atque ducimus velit, earum quidem, iusto dolorem. </p>
+                        </div>
+                    </div>
+                </div><!-- End off col-md-6 -->
+            </div>
+        </div><!-- End off row -->
+    </div><!-- End off container -->
+</section><!-- End off choose section -->
---
+++ resources/views/partials/test.blade.php
@@ -0,0 +1,57 @@
+<!--Test section-->
+<section id="test" class="test bg-grey roomy-60 fix">
+    <div class="container">
+        <div class="row">
+            <div class="main_test fix">
+                <div class="col-md-6">
+                    <div class="cbrand text-center">
+                        <div class="divider_horizontal"></div>
+                        <div class="divider_vertical"></div>
+                        <div class="col-xs-6">
+                            <div class="cbrand_item">
+                                <img src="assets/images/cbrand-1.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="cbrand_item">
+                                <img src="assets/images/cbrand-2.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="cbrand_item m-top-40">
+                                <img src="assets/images/cbrand-3.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="cbrand_item m-top-40">
+                                <img src="assets/images/cbrand-4.png" alt="" />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-6 sm-m-top-40">
+                    <div class="test_item1 fix ">
+                        <div class="head_title fix">
+                            <h2 class="text-uppercase">What <strong>Client Say</strong></h2>
+                        </div>
+                        <div class="item_img">
+                            <img class="img-circle" src="assets/images/test-img1.jpg" alt="" />
+                            <i class="fa fa-quote-left"></i>
+                        </div>
+
+                        <div class="item_text">
+                            <h5>Sarah Smith</h5>
+                            <h6>envato.com</h6>
+
+                            <p>Natus voluptatum enim quod necessitatibus quis
+                                expedita harum provident eos obcaecati id culpa
+                                corporis molestias.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- End off test section -->
